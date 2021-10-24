85802946203364265748f57f4645682cfe1698e7
Ian Alves <53923227+ian-alves@users.noreply.github.com>
2021-10-24 20:44:57 -0300

    updates

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -6,6 +6,17 @@ type AuthProviderProps = {
     children: ReactNode;
 }
 
+type User = {
+    id: string,
+    name: string,
+    login: string,
+    avatar_url: string
+}
+
+type AuthContextData = {
+    user: User
+}
+
 export function AuthProvider(props: AuthProviderProps) {
     return (
         <AuthContext.Provider value={null}>
